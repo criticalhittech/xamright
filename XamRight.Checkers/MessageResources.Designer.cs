521995a37b20abb67a1240f22a654bdbdf2d6261
David Sielaff <david@criticalhittech.com>
2023-02-17 17:04:04 -0800

    Fix message resources

--- XamRight.Checkers/MessageResources.Designer.cs
+++ XamRight.Checkers/MessageResources.Designer.cs
@@ -20,7 +20,7 @@ namespace XamRight.Checkers {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class MessageResources {
@@ -85,6 +85,15 @@ namespace XamRight.Checkers {
         internal static string BindableLayoutItemTemplateWarning {
             get {
                 return ResourceManager.GetString("BindableLayoutItemTemplateWarning", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to IsEnabled on Button with Command may misbehave; prefer Command.CanExecute to IsEnabled.
+        /// </summary>
+        internal static string ButtonCommandIsEnabledWarning {
+            get {
+                return ResourceManager.GetString("ButtonCommandIsEnabledWarning", resourceCulture);
             }
         }
         
